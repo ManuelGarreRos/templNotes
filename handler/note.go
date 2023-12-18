2a184dfc80bde023c2e9092786d13d25b549c770
mgarre <mgrdesarrollo@gmail.com>
2023-12-18 14:00:50 +0100

    style: add tailwind css feat: automake on save

--- handler/note.go
+++ handler/note.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/ManuelGarreRos/templNotes/model"
 	"github.com/ManuelGarreRos/templNotes/view/note"
 	"github.com/labstack/echo/v4"
 )
@@ -8,5 +9,23 @@ import (
 type NoteHandler struct{}
 
 func (n *NoteHandler) GetNotes(c echo.Context) error {
-	return render(c, note.ShowNotes())
+	newNote := []model.Note{
+		{
+			ID:    1,
+			Title: "My first note",
+			Body:  "This is the body of my first note",
+		},
+		{
+			ID:    2,
+			Title: "My second note",
+			Body:  "This is the body of my second note",
+		},
+		{
+			ID:    3,
+			Title: "My third note",
+			Body:  "This is the body of my third note",
+		},
+	}
+
+	return render(c, note.ShowNotes(newNote))
 }
